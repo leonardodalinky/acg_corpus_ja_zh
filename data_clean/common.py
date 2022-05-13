e2cf764be59771b3fff6363176e57a22f413bde7
leonardodalinky <493987054@qq.com>
2022-05-14 02:30:31 +0800

    feat: load epub

---
+++ data_clean/common.py
@@ -0,0 +1,52 @@
+from typing import *
+
+__all__ = [
+    "Document",
+    "filter_docs_by_threshold"
+]
+
+
+class Document:
+    __REPR_TEXT_LEN__ = 20
+
+    def __init__(
+        self, text: str, title: Optional[str] = None, chapter_id: Optional[str] = None
+    ):
+        assert text is not None
+        self.text = text
+        self.title = title
+        self.chapter_id = chapter_id
+
+    def __len__(self):
+        return len(self.text)
+
+    def __str__(self):
+        return self.text
+
+    def __repr__(self):
+        return f"<Document(title={self.title}, chapter_id={self.chapter_id}, len={len(self)}, text=\"{self.text[:self.__REPR_TEXT_LEN__]}\")>"
+
+
+def filter_docs_by_threshold(
+    docs: List[Document], threshold: float = 0.05, min_keep_len: int = 1000,
+) -> List[Document]:
+    """
+    Filter documents by the ratio of length. The percentage of documents that are filtered out is less than threshold.
+
+    :param docs:
+    :param threshold: the percentage of documents that are dropped.
+    :param min_keep_len: if the length of a document is greater than this value, it will be kept.
+    :return:
+    """
+    assert 0.0 < threshold < 1.0
+    assert len(docs) > 0
+    asc_docs = sorted(docs, key=lambda doc: len(doc))
+    all_len = sum(len(doc) for doc in docs)
+    filtered_docs = []
+    for doc in asc_docs:
+        if len(doc) > min_keep_len:
+            break
+        filtered_docs_len = sum(len(doc) for doc in filtered_docs)
+        if (filtered_docs_len + len(doc)) / all_len < threshold:
+            filtered_docs.append(doc)
+    return [doc for doc in docs if doc not in filtered_docs]
---
+++ data_clean/epub.py
@@ -0,0 +1,90 @@
+import os
+import re
+import ebooklib
+import unicodedata
+from ebooklib import epub
+from bs4 import BeautifulSoup
+from pathlib import Path
+from typing import *
+from .common import Document
+
+__all__ = ["read_docs_from_epub"]
+
+
+def read_docs_from_epub(
+    file_path: Union[str, bytes, os.PathLike]
+) -> List[Document]:
+    """
+    Read epub file and return a list of documents. Each document corresponds to a chapter.
+
+    :param file_path: path to epub file
+    :return:
+    """
+    ret = []
+    for doc in _read_epub_to_text(file_path):
+        ret.append(
+            Document(
+                text=doc["text"],
+                title=doc["title"],
+                chapter_id=doc["file_stem"],
+            )
+        )
+    return ret
+
+
+def _read_epub_to_text(
+    file_path: Union[str, bytes, os.PathLike]
+) -> List[Dict[str, Any]]:
+    """
+    Read epub file and return a list of text. Each text corresponds to a chapter.
+
+    :param file_path: path to epub file
+    :return:
+        list of dict: {
+            "title": str,
+            "text": str or None,
+            "file_stem": str or None,
+        }
+    """
+    ebook = epub.read_epub(file_path)
+    ret = []
+    for uid, _ in ebook.spine:
+        uid: str
+        doc: epub.EpubItem = ebook.get_item_with_id(uid)
+        if doc is None or doc.get_type() != ebooklib.ITEM_DOCUMENT:
+            continue
+        file_stem = Path(doc.get_name()).stem
+        title = None
+        for link in ebook.toc:
+            if link.href.find(file_stem) != -1:
+                title = _normalize_epub_text(link.title)
+                break
+        soup = BeautifulSoup(doc.get_content(), "lxml")
+        ret.append(
+            {
+                "title": title if title is not None and len(title) > 0 else None,
+                "file_stem": file_stem if file_stem is not None and len(file_stem) > 0 else None,
+                "text": _normalize_epub_text(soup.get_text()),
+            }
+        )
+    return ret
+
+
+def _normalize_epub_text(text: Optional[str]) -> str:
+    """
+    Normalize epub text.
+
+    :param text: epub text
+    :return: normalized text
+    """
+    if text is None:
+        return ""
+    # replace multiple spaces with single space
+    text = re.sub(r" +", " ", text)
+    # replace \r
+    text = re.sub(r"\r", "", text)
+    # replace multiple new lines with single new line
+    text = re.sub(r"\n+", "\n", text)
+    # unicode normalization
+    text = unicodedata.normalize("NFKC", text)
+    return text.strip()
